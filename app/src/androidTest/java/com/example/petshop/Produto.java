f83acfc0d9de40f5732728f0b2789694aa9d3e88
AnnaFragoso <090501ab>
2019-11-13 20:47:10 -0300

    Inicio do Retrofit, consumo da API p/ listagem de produtos

---
+++ app/src/androidTest/java/com/example/petshop/Produto.java
@@ -0,0 +1,78 @@
+package com.example.petshop;
+
+public class Produto {
+    private String nomeProduto;
+    private float descProduto;
+    private float precProduto;
+    private int idCategoria;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public float getDescProduto() {
+        return descProduto;
+    }
+
+    public void setDescProduto(float descProduto) {
+        this.descProduto = descProduto;
+    }
+
+    public float getPrecProduto() {
+        return precProduto;
+    }
+
+    public void setPrecProduto(float precProduto) {
+        this.precProduto = precProduto;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public int getQtdMinEstoque() {
+        return qtdMinEstoque;
+    }
+
+    public void setQtdMinEstoque(int qtdMinEstoque) {
+        this.qtdMinEstoque = qtdMinEstoque;
+    }
+
+    public boolean isAtivoProduto() {
+        return ativoProduto;
+    }
+
+    public void setAtivoProduto(boolean ativoProduto) {
+        this.ativoProduto = ativoProduto;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public float getDescontoPromocao() {
+        return descontoPromocao;
+    }
+
+    public void setDescontoPromocao(float descontoPromocao) {
+        this.descontoPromocao = descontoPromocao;
+    }
+
+    private int qtdMinEstoque;
+    private boolean ativoProduto;
+    private int idProduto;
+    private float descontoPromocao;
+}
+
---
+++ app/src/main/java/com/example/petshop/ListagemActivity.kt
@@ -0,0 +1,16 @@
+package com.example.petshop
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+class ListagemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listagem)
+
+    }
+}
